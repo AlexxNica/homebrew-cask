df45ae44aa424feebc438f185b81a34974b1916c
Alexandre Nicastro <kryndex@gmail.com>
2017-03-12 15:48:18 -0300

    Update to 7.2.3 [latest from: March 12]

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,6 +1,6 @@
 cask 'razorsql' do
-  version '7.1.0'
-  sha256 'cabc57ed66062e03250b321ac486a4b484f22bb502af80a5fe7c491dd4e9a9d0'
+  version '7.2.3'
+  sha256 '9d4e2131c06887d2ca32191473bd1b3427c8fb08c396eb8c6e4873be4605e9d6'
 
   url "http://downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}_x64.dmg"
   name 'RazorSQL'
